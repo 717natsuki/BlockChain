22e99f72cea3bb2aca26d87cd9af31c27a2caeea
Natsuki Kataoka <717natsuki@gmail.com>
2020-03-12 16:28:19 +0900

    no message

--- blockchain/dev/test.js
+++ blockchain/dev/test.js
@@ -5,14 +5,7 @@ const bitcoin = new BlockChain();
 bitcoin.createNewBlock(2842, 'Natsuki', 'other person  ');
 bitcoin.createNewTransaction(9232,'you','me')
 
-bitcoin.createNewBlock(1432, 'IDSJOS', 'OOFOSSSDKSD');
-bitcoin.createNewTransaction(8232,'you','me')
-
-bitcoin.createNewBlock(084, 'DOISODSISVSDG', 'SDIOOS');
-bitcoin.createNewTransaction(1232,'you','me')
-bitcoin.createNewTransaction(2232,'you','me')
-bitcoin.createNewTransaction(3232,'you','me')
-bitcoin.createNewBlock(084, 'DOISODSISVSDG', 'SDIOOS');
 
 
-console.log(bitcoin.chain[3]);
+
+console.log(bitcoin.hashBlock('OSDIJFOS', bitcoin.chain[0], 32));
